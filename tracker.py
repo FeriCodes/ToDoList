d1759e0b799b0310153fe84e007a94bf00fe28a3
FeriCodes <hosseinbigdeli335@gmail.com>
2026-05-23 23:18:08 +0330

    feat: Implement core functions for To-Do list application and fix gitignore filename

---
+++ tracker.py
@@ -0,0 +1,51 @@
+import json
+
+
+def main_menu():
+    print("\n----------Main Menu----------\n")
+    print("1-Add task")
+    print("2-Mark task done")
+    print("3-View calendar and tasks")
+    print("4-Remove task")
+    print("5-View streaks")
+    print("6-Exit")
+
+
+def add_task():
+    """
+    1-Adds tasks to help build streaks and habits. tasks are also saved to a json file.
+    """
+
+
+def mark_task_done():
+    """
+    2-Marks a task as completed. Visually indicates completion (e.g., with a green mark) or non-completion (e.g., with a red mark) if the task was not done.
+    """
+
+
+def view_calendar():
+    """
+    3-Creates a simple calendar view and displays all associated tasks.
+    """
+
+
+def remove_task():
+    """
+    4-Remove the selected task from the program.
+    """
+
+
+def view_streaks():
+    """
+    5-Displays all tracked streaks for the user's tasks.
+    """
+
+
+def Exit():
+    """
+    6-Close the program.
+    """
+
+
+if __name__ == "__main__":
+    main()
